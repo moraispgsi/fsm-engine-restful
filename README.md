9a66c5ce7c9cf684339af572d7537e3c0cdf1274
moraispgsi <moraispgsi@gmail.com>
2017-07-10 14:26:14 +0100

    new server

--- README.md
+++ README.md
@@ -1,15 +1,48 @@
-# fsm-engine-restful
-[![Build Status](https://travis-ci.org/moraispgsi/fsm-engine-restful.svg?branch=master)](https://travis-ci.org/moraispgsi/fsm-engine-restful)
+# node-express-sequelize-es2015
 
-A RESTful API for the fsm-engine module
+A boilerplate using NodeJs, Express, Sequelize, Apidoc, Eslint, Mocha, Cluster and the best practices.
 
+## Getting up and running
 
-## Environmental Variables
+1. Clone the repository
+2. `npm install`
+3. `npm run dev`
+4. Visit `https://localhost:3000`
+5. Visit `https://localhost:3000/apidoc` to see the existing API
 
-DIALECT //the sequelize database type  
-DATABASE_HOST //the database host  
-DATABASE_PORT //the database port  
-DATABASE_USER  //the database user  
-DATABASE_PASSWORD  //the database password  
-DATABASE_NAME //the database name  
-ACTION_DISPATCHER_HOST //the action dispatcher host  
+## Some command
+
+```
+npm start  # Run the serveur in cluster mode (it also generate the apidoc)
+npm lint   # Check lint error using eslint
+npm test   # run mocha tests
+npm apidoc # Generate the apidoc
+```
+
+## Coding Style
+
+The coding style used is the airbnb one: https://github.com/airbnb/javascript
+
+You can configure it in the `.eslintrc` file.
+
+## Architecture
+
+The architecture is MVR (Model View Route)
+
+The files are structured in the following manner:
+```
+libs        (Configurations files and libs)
+models      (Sequelize models)
+routes      (Express routes, the comments in this folder are used to generate the apidoc)
+test        (mocha test, 'npm test' to run it)
+auth.js     (Configuration for JWT auth (JSON Web Tokens))
+cluster.js  (Used to run in cluster mode. We could use https://github.com/Unitech/pm2 too)
+index.js    (Entry point)
+ntask.*     (SSL certificates)
+```
+
+If you want to have a better understanding of the architecture, I recommend you to read [Caio Ribeiro Pereira's book](https://leanpub.com/building-apis-with-nodejs).
+
+## Credit
+
+This project is base on [the book of Caio Ribeiro Pereira](https://leanpub.com/building-apis-with-nodejs) and [his repository](https://github.com/caio-ribeiro-pereira/building-apis-with-nodejs) so, all the credit goes to him. Also, I recommend you his book.
---
+++ old/index.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Ricardo Morais on 14/04/2017.
+ */
+
+
+//    ad88                                                                                 88
+//   d8"                                                                                   ""
+//   88
+// MM88MMM  ,adPPYba,  88,dPYba,,adPYba,              ,adPPYba,  8b,dPPYba,    ,adPPYb,d8  88  8b,dPPYba,    ,adPPYba,
+//   88     I8[    ""  88P'   "88"    "8a  aaaaaaaa  a8P_____88  88P'   `"8a  a8"    `Y88  88  88P'   `"8a  a8P_____88
+//   88      `"Y8ba,   88      88      88  """"""""  8PP"""""""  88       88  8b       88  88  88       88  8PP"""""""
+//   88     aa    ]8I  88      88      88            "8b,   ,aa  88       88  "8a,   ,d88  88  88       88  "8b,   ,aa
+//   88     `"YbbdP"'  88      88      88             `"Ybbd8"'  88       88   `"YbbdP"Y8  88  88       88   `"Ybbd8"'
+//                                                                             aa,    ,88
+//                                                                              "Y8bbdP"
+//                                                ad88               88
+//                                      ,d       d8"                 88
+//                                      88       88                  88
+// 8b,dPPYba,   ,adPPYba,  ,adPPYba,  MM88MMM  MM88MMM  88       88  88
+// 88P'   "Y8  a8P_____88  I8[    ""    88       88     88       88  88
+// 88          8PP"""""""   `"Y8ba,     88       88     88       88  88
+// 88          "8b,   ,aa  aa    ]8I    88,      88     "8a,   ,a88  88
+// 88           `"Ybbd8"'  `"YbbdP"'    "Y888    88      `"YbbdP'Y8  88
+//
+
+let co = require('co');
+let Engine = require("fsm-engine");
+
+co(function*(){
+    let engine = new Engine(process.env.ACTION_DISPATCHER_HOST, __dirname + "/repo");
+
+    let express = require('express');
+    let app = express();
+    let bodyParser = require('body-parser');
+
+    app.use(bodyParser.json());         // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }));
+
+    // initialize the oauth2
+    require('./oauth2.js')(app);
+    app.use(express.static('doc'));
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+
+    require("./webservice")(app, engine);
+
+    //Start the server
+    let server = app.listen(process.env.PORT || 8081, process.env.HOST || '0.0.0.0', function () {
+        let host = server.address().address;
+        let port = server.address().port;
+        console.log("listening at http://%s:%s", host, port)
+    });
+
+    //The server needs to be operational in order to bind the port within 90 seconds
+    //Therefore since the engine init is an expensive operation, we initialize the server first.
+    yield engine.init(process.env.CLONE_URL, process.env.PUBLIC_KEY, process.env.PRIVATE_KEY, process.env.PASSPHRASE);
+
+}).then().catch(function(err){
+    console.log(err);
+});
+
