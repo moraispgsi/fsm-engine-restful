4a9f51cad1a0d9fe245303024538465e9fad3429
moraispgsi <moraispgsi@gmail.com>
2017-07-18 23:17:22 +0100

    start.js

--- build/index.js
+++ build/index.js
@@ -12,7 +12,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
-(0, _consign2.default)({ verbose: false }).include('./libs/config.js').then('./db.js').then('auth.js').then('./libs/middlewares.js').then('./routes').then('./libs/boot.js').into(app);
+(0, _consign2.default)({ verbose: false, cwd: __dirname }).include('libs/config.js').then('db.js').then('auth.js').then('libs/middlewares.js').then('routes').then('libs/boot.js').into(app);
 
 module.exports = app;
 //# sourceMappingURL=index.js.map
